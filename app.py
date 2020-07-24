9638c9703f06452e0e0386f42650b5403020eb3f
Saheed Lamina git config --global user.email you@example.comgit config --global user.name Saheed <agbolamina@hotmail.com>
2020-07-24 15:53:21 +0100

    Added functionality to log in and see welcome page

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, url_for
 from flask import Flask, flash, redirect, render_template, request, session, abort
 import os
 
-# import flask
+
 #  the below code creates an instance of the app
 
 app = Flask(__name__)
@@ -13,20 +13,55 @@ def home():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        return "Hello Boss!"
+        return "Did it work???"
 
-@app.route('/login', methods=['POST'])
+#
+# @app.route("/login")
+# def login():
+#     return render_template("login.html")
+
+
+@app.route('/login', methods=['GET','POST'])
 def do_admin_login():
     if request.form['password'] == 'password' and request.form['username'] == 'admin':
-        session['logged_in'] = True
+        # session['logged_in'] = True
+        return redirect(url_for('welcome'))
     else:
         flash('wrong password!')
         return home()
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'password':
+            attempt = int(session.get('attempt'))
+            if attempt == 2:
+                flash("This is your last chance")
+                # attempt += 1
+                # session['attempt'] = attempt
+            if attempt == 3:
+                flash('You made too many incorrect login attempts. Please contact an administrator')
+                abort(404)
+            else:
+                attempt += 1
+                session['attempt'] = attempt
+                error = 'Invalid Credentials. Please try again'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in.')
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
 @app.route("/logout")
 def logout():
     session['logged_in'] = False
     return home()
+
+@app.route("/welcome")
+def welcome():
+    return render_template("welcome.html")
+
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
